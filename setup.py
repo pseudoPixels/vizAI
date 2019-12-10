e5b3e4d51d6c2a68e6afce396f02cae36000a239
Golam Mostaeen <golam.mostaeen@gameloft.com>
2019-12-10 10:21:15 -0500

    setup script added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='vizAI',
+      version='0.0.1',
+      description="vizAI",
+      author='Golam Mostaeen',
+      author_email='golammostaeen@gmail.com',
+      license='',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[]
+      )
