eafd7d8b17d3b4c741cbff843930d793d158dad8
Golam Mostaeen <golam.mostaeen@gameloft.com>
2019-12-10 16:00:50 -0500

    setup.py updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='vizAI',
-      version='0.0.1',
+      version='0.0.2',
       description="vizAI",
       author='Golam Mostaeen',
       author_email='golammostaeen@gmail.com',
