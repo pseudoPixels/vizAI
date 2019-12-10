48dedeca17ce4f2bb729225870fa28770259d9c3
Golam Mostaeen <golam.mostaeen@gameloft.com>
2019-12-10 15:22:10 -0500

    sphinx docs init

---
+++ docs/source/index.rst
@@ -0,0 +1,28 @@
+.. vizAI documentation master file, created by
+   sphinx-quickstart on Tue Dec 10 15:05:42 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to vizAI's documentation!
+=================================
+
+
+.. automodule:: vizAI.core.test
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- vizAI/core/test.py
+++ vizAI/core/test.py
@@ -4,5 +4,21 @@ import numpy as np
 from plotnine import *
 from plotnine.data import mpg
 
-p = ggplot(mpg) + geom_bar(aes(x='class', fill='drv') ) + theme_minimal()
-p.save(filename='../webapp/static/images/sampleChart.png', height=8, width=20)
+
+class Test:
+    def __init__(self):
+        pass
+
+    def make_simple_plot(self,x='class', target_filename='../webapp/static/images/sampleChart.png', height=8, width=20):
+        """
+        Creates a simple plot for test purpose.
+
+        :param x: The column name for X axis in the plot.
+        :param target_filename: The target filepath to save the generated visualization image
+        :param height: Height of the image for saving
+        :param width: Width of the image for saving
+        :return: None, saves the image
+        """
+
+        p = ggplot(mpg) + geom_bar(aes(x=x) ) + theme_minimal()
+        p.save(filename=target_filename, height=height, width=width)
