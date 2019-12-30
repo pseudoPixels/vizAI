eaea1ac7737b987528821a61cfe5f93f9cb22320
pseudoPixels <codevision.source@gmail.com>
2019-12-30 16:07:10 -0500

    drag input first version added

--- vizAI/webapp/static/js/plot_controllers/draw_plot.js
+++ vizAI/webapp/static/js/plot_controllers/draw_plot.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
 
 
     $(document).on('change', ".plot_feature" ,function () {//here
-        //alert($(this).val());
-
-        //get the ouput list via async call
     	$.ajax({
             type: "POST",
             cache: false,
@@ -21,6 +18,36 @@ $(document).ready(function(){
 
     	});
 
+
+    });
+
+
+
+    $( ".aFeature" ).draggable({helper:'clone'});
+
+    $(".featureDroppable").droppable({
+        accept:'.aFeature',
+        drop: function(ev, ui){
+            var droppedItem = $(ui.draggable).clone();
+            $(this).html("<h4><b>"+$(ui.draggable).text()+"</b></h4>");
+
+            $.ajax({
+                type: "POST",
+                cache: false,
+                url: "/getPlot",
+                data: "color="+$(ui.draggable).text(),
+                success: function (option) {
+                    //alert(option['plotData']);
+                    var figure = JSON.parse(option['plotData']);
+                    Plotly.newPlot('myDiv', figure.data, figure.layout);
+                },
+                error: function (xhr, status, error) {
+                        alert(xhr.responseText);
+                }
+
+    	    });
+
+        }
 
     });
 
