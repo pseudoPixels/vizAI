e0f8ea888923d522ba46e95d1513d2861feb881c
pseudoPixels <codevision.source@gmail.com>
2020-04-21 00:33:43 -0400

    Graphaite Project Model init

---
+++ graphaite/core/models/GraphaiteProject.py
@@ -0,0 +1,29 @@
+from flaskext.couchdb import (
+    Document,
+    TextField,
+    FloatField,
+    DictField,
+    Mapping,
+    ListField,
+    IntegerField,
+)
+
+
+class GraphaiteProjectModel(Document):
+    doc_type = "graphaite_project"
+
+    project_id = TextField()  ## unique graph id
+    project_title = TextField()  ## title of the graph
+    porject_owner_id = TextField()  ## figure data, like json for plotly
+    # last_modified_date = DateTimeField(default=datetime.datetime.now)
+
+    ### Graph Atributes
+    dataset_path = TextField()
+    selected_feature_variables = ListField(TextField())
+    selected_target_variable = TextField(default="")
+
+    graphaite_graph_ids = ListField(TextField())
+
+
+if __name__ == "__main__":
+    pass
